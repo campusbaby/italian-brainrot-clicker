1d0e656b098642114b1a1778fab0992ef8ca7696
campusbaby <824377536@qq.com>
2025-05-14 12:18:25 +0800

    修复：强制设置游戏iframe为英文和美国区域，解决地理位置检测导致显示西班牙语的问题

--- src/components/game/GameFrame.tsx
+++ src/components/game/GameFrame.tsx
@@ -6,7 +6,7 @@ export default function GameFrame() {
   return (
     <div className="w-full h-full relative bg-gray-900 rounded-lg overflow-hidden">
       <iframe 
-        src="https://www.crazygames.com/embed/italian-brainrot-clicker-usp?locale=en" 
+        src="https://www.crazygames.com/embed/italian-brainrot-clicker-usp?locale=en&lang=en&countryOverride=US" 
         style={{ width: '100%', height: '100%' }} 
         frameBorder="0" 
         allow="gamepad *; fullscreen *; autoplay; clipboard-write; encrypted-media;" 
